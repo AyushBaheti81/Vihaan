276e28469b590dc5df8e1b40ddbef984b563b85c
Ayush <ayushbaheti81@gmail.com>
2020-05-16 16:49:36 +0530

    t2s integrated with camera support(low accuracy)

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import time
+
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
+
+import t2s
 
 
 class MainWindow(Screen):
@@ -11,14 +15,23 @@ class MainWindow(Screen):
 class SecondWindow(Screen):
     pass
 
-class ThirdWindow(Screen):
-    pass
 
 class WindowManager(ScreenManager):
     pass
 
-class CameraClick(BoxLayout,Screen):
-    pass
+
+class CameraClick(BoxLayout, Screen):
+    def capture(self):
+        '''
+        Function to capture the images and give them the names
+        according to their captured time and date.
+        '''
+        camera = self.ids['camera']
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        clickedPhoto = "IMG_{}.jpg".format(timestr)
+        camera.export_to_png(clickedPhoto)
+        t2s.convertAudio(clickedPhoto)
+        print("Captured")
 
 kv = Builder.load_file("my.kv")
 
